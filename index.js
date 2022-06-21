d43dd9f0c080e9c87587b5d88fcb25747f928d07
Spencer Mathews <smathews0906@gmail.com>
2022-06-21 14:05:52 -0500

    add events

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+//fetch
+fetch('https://randomuser.me/api/')
+    .then(res=>res.json())
+    .then(randomUser=>{
+        console.log(randomUser)
+        const generateNewUserBtn = document.querySelector(".generator")
+        generateNewUserBtn.addEventListener("click", ()=>{
+            generateNewUser(randomUser)
+            generateNewUserBtn.remove()
+        })
+        
+    })
+
+//generate new user
+function generateNewUser(user){
+    const enemyName = document.querySelector(".name")
+    const enemyAge = document.querySelector(".age")
+    const enemyImage= document.querySelector(".image")
+    const enemyLoc= document.querySelector(".address")
+    enemyName.textContent = user.results[0].name.first
+    enemyImage.src=user.results[0].picture.large
+    enemyAge.textContent= `Age: ${user.results[0].dob.age}`
+    enemyLoc.textContent= `Location: ${user.results[0].location.state}`
+}
+
+
+
+
+//Accept and Decling mouseenter and mouseleave events
 const acceptBtn=document.querySelector(".accept")
 const enemyCard=document.querySelector(".enemyCard")
 const declineBtn= document.querySelector(".decline")
@@ -15,4 +44,14 @@ declineBtn.addEventListener("mouseenter", ()=>{
 
 declineBtn.addEventListener("mouseleave", ()=>{
     enemyCard.style.background= "black"
+})
+
+// accept button click
+acceptBtn.addEventListener("click", ()=>{
+    acceptBtn.remove()
+    declineBtn.remove()
+    const congrats= document.createElement("p")
+    const address= document.querySelector(".address")
+    congrats.textContent="Congrats, you have 1 new enemy!"
+    address.append(congrats)
 })
