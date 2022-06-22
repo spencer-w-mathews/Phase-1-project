5be769bc17280c0985987395c563494e971e8a7d
Spencer Mathews <smathews0906@gmail.com>
2022-06-21 19:11:04 -0500

    prettifying

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ fetch('https://randomuser.me/api/')
                 declineBtn.remove()
                 const congrats= document.createElement("p")
                 const address= document.querySelector(".address")
-                        congrats.textContent=`Congrats, ${randomUser.results[0].name.first} is your Archenemy!`
+                        congrats.textContent=`${randomUser.results[0].name.first} is your Archenemy!`
                 address.append(congrats)
+                const form = document.querySelector(".message")
+                form.addEventListener("submit", (e)=>{
+                    form.reset()
+                    e.preventDefault()
+                    alert('You sent your enemy a message!')
+                })
             })
         })
         
@@ -38,20 +44,25 @@ function generateNewUser(user){
 const acceptBtn=document.querySelector(".accept")
 const enemyCard=document.querySelector(".enemyCard")
 const declineBtn= document.querySelector(".decline")
+
 acceptBtn.addEventListener("mouseenter", ()=>{
     enemyCard.style.background= "green"
+    acceptBtn.style.color= "green"
 })
 
 acceptBtn.addEventListener("mouseleave", ()=>{
     enemyCard.style.background= "black"
+    acceptBtn.style.color= "black"
 })
 
 declineBtn.addEventListener("mouseenter", ()=>{
     enemyCard.style.background= "red"
+    declineBtn.style.color= "red"
 })
 
 declineBtn.addEventListener("mouseleave", ()=>{
     enemyCard.style.background= "black"
+    declineBtn.style.color= "black"
 })
 
 // accept button click
@@ -65,9 +76,9 @@ declineBtn.addEventListener("mouseleave", ()=>{
 // })
 
 // submit button
-const form = document.querySelector(".message")
-form.addEventListener("submit", (e)=>{
-    form.reset()
-    e.preventDefault()
-    alert('You sent your enemy a message!')
-})
+// const form = document.querySelector(".message")
+// form.addEventListener("submit", (e)=>{
+//     form.reset()
+//     e.preventDefault()
+//     alert('You sent your enemy a message!')
+// })
