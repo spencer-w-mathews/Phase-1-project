d6ee9da32c6f0e0576bdf7e171497bd3b191bf2d
Spencer Mathews <smathews0906@gmail.com>
2022-06-22 08:57:37 -0500

    minor

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ fetch('https://randomuser.me/api/')
                 phone.append(congrats)
 
                 const randomly = () => Math.random() - 0.5
-                const weakness = ['Weakness: Bad Knees', 'Weakness: Stepping on Legos', 'Weakness: Time Zones', 'Weakness: Looking Both Ways Down A One Way Road', 'Weakness: Having Name Spelled Backwards', 'Tree Nut Allergy', 'Poor Internet Connection', 'Does Not Take Criticism Well', 'Commitment', 'The Color Yellow', 'Shy in Large Groups', 'Cares Too Much', 'Sarcasm']
+                const weakness = ['Weakness: Bad Knees', 'Weakness: Stepping on Legos', 'Weakness: Time Zones', 'Weakness: Looking Both Ways Down A One Way Road', 'Weakness: Having Name Spelled Backwards', 'Weakness: Tree Nut Allergy', 'Weakness: Poor Internet Connection', 'Weakness: Does Not Take Criticism Well', 'Weakness: Commitment', 'Weakness: The Color Yellow', 'Weakness: Shy in Large Groups', 'Weakness: Cares Too Much', 'Weakness: Sarcasm']
                 const weaknessInfo = Array(1).fill({})
                 const dynamicWeakness = [].concat(weakness).sort(randomly)
                 weaknessInfo.forEach((t, i)=> {
